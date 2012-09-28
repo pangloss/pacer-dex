34f848418c2bdfa4b7467794e13943405c68f054
Darrick Wiebe <dw@xnlogic.com>
2012-09-28 17:07:54 -0400

    Clean up better on shutdown

--- lib/pacer-dex/graph.rb
+++ lib/pacer-dex/graph.rb
@@ -15,7 +15,7 @@ module Pacer
       end
       shutdown = proc do |g|
         g.blueprints_graph.shutdown
-        Pacer.open_graphs[path] = nil
+        Pacer.open_graphs.delete path
       end
       PacerGraph.new(Pacer::Dex::Encoder, open, shutdown)
     end
